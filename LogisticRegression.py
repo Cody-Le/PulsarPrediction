4ff0d46e986a8742dd4a1627a3a1a5bfa2bde255
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2022-12-11 20:23:05 -0500

    Add files via upload

---
+++ LogisticRegression.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def featureScale(x):
+
+    average = np.mean(x, axis=0)
+    range = np.max(x, axis = 0) - np.min(x, axis = 0)
+    return (x-average)/range
+
+
+class logisticRegression():
+    def __init__(self, inputSize):
+        self.theta = np.random.rand(inputSize + 1,1) - 0.5
+        self.inputShape = inputSize + 1
+
+        #print(self.theta)
+
+
+
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def predict(self, x):
+        X = np.ones((x.shape[0], x.shape[1] + 1))
+
+        X[:,1:] = x
+
+        linearHypothesis = np.dot(X,self.theta)
+        return self.sigmoid(linearHypothesis)
+
+    def cost(self, y_pred, y):
+        totalCost = -1 * np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+        return totalCost
+
+
+
+    def training(self, x, y, epochs, learningRate):
+        X = np.ones((x.shape[0], x.shape[1] + 1))
+
+        X[:, 1:] = x
+        costOverTime = np.zeros(epochs)
+        for epoch in range(epochs):
+            newTheta = np.zeros(self.theta.shape)
+            for j in range(self.inputShape):
+                gradient = np.mean((self.predict(x) - y)* X[:,j][:, np.newaxis])
+
+                newTheta[j] = self.theta[j] -  learningRate * gradient
+            self.theta = newTheta
+
+            print(self.cost(self.predict(x), y))
+            costOverTime[epoch] = self.cost(self.predict(x), y)
+        plt.figure()
+        plt.plot(np.arange(0, epoch + 1), costOverTime)
+        plt.show()
+
+
+
+
+#Processing Data
+
+df = pd.read_csv("./pulsar_data_train.csv")
+df_np = df.to_numpy()
+X_train = df.iloc[:,:-1].to_numpy()
+y_train = df.iloc[:,-1].to_numpy()
+
+#Clean data
+X_train[np.isnan(X_train)] = 0
+
+X_scaled_train = featureScale(X_train)
+
+
+
+model = logisticRegression(8)
+y_pred = model.predict(X_scaled_train)
+#print(model.cost(y_pred, y_train[:, np.newaxis]))
+model.training(X_scaled_train, y_train[:, np.newaxis], 10000, 0.05)
